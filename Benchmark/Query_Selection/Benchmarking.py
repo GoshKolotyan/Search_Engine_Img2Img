0710ffee7531d5993db620a2790e37fcce44cb0c
GoshKolotyan <goshkolotyan@gmail.com>
2025-02-24 18:02:16 +0400

    Adding Benchmarking logic version 1

---
+++ Benchmark/Query_Selection/Benchmarking.py
@@ -0,0 +1,18 @@
+import json 
+import numpy as  np
+
+class Benchmark:
+    def __init__(self):
+        pass
+
+    def precision_at_k(retrieved, relevant, k):
+        pass 
+
+    def recall_at_k(retrieved, relevant, k):
+        pass 
+    
+    def average_precision(retrieved, relevant):
+        pass 
+
+    def mean_average_precision(retrieved, relevant):
+        pass 
