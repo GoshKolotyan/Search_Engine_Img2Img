42829059bcbb7c149879136aaad2a6ee2fc32137
GoshKolotyan <goshkolotyan@gmail.com>
2025-03-04 17:27:32 +0400

    adding croping idea

--- RBenchmarking/benchmark.py
+++ RBenchmarking/benchmark.py
@@ -3,7 +3,7 @@ import csv
 import glob
 import torch
 import numpy as np
-import pandas as pd 
+import pandas as pd
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import logging
@@ -11,14 +11,14 @@ from PIL import Image
 from torchvision import transforms, models
 
 os.makedirs("plots", exist_ok=True)
-os.makedirs("logs", exist_ok=True) 
+os.makedirs("logs", exist_ok=True)
 
 logging.basicConfig(
     format="{asctime} - {levelname} - {message}",
     level=logging.INFO,
     style="{",
     datefmt="%Y-%m-%d %H:%M",
-    filename='./logs/errores.log'  
+    filename="./logs/errores.log",
 )
 
 logging.info("Application started successfully.")
@@ -27,7 +27,7 @@ logging.info("Application started successfully.")
 class RBenchmarking:
     def __init__(self, folder_path, model_name):
         self.folder_path = folder_path
-        self.original_image_path = self.folder_path + '/original.jpg'
+        self.original_image_path = self.folder_path + "/original.jpg"
         self.model_name = model_name
 
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -123,25 +123,39 @@ class RBenchmarking:
         # plt.show()
 
     def augment_image(self, image: Image.Image) -> dict:
+        width, height = image.size
+
+        img_left_area = (0, 0, width//2, height)
+        img_right_area = (width//2, 0, width, height)
+        img_top_area = (0, 0, width, height//2) 
+        img_bottom_area = (0, height//2, width, height)  
 
         augmentations = {
             "original": image,
             "flipped_horizontally": image.transpose(Image.FLIP_LEFT_RIGHT),
             "rotated_20": image.rotate(20),
+            "cropped_from_left": image.crop(img_left_area),
+            "cropped_from_right": image.crop(img_right_area),
+            "cropped_from_top": image.crop(img_top_area), 
+            "cropped_from_bottom": image.crop(img_bottom_area) 
         }
         return augmentations
+
+
     def _record_to_csv(self, similarity_scores, model_name):
         save_path = f"./plots/{self.folder_path.split('/')[-1]}"
         csv_filename = f"{save_path}/{self.folder_path.split('/')[-1]}_records.csv"
 
-        with open(csv_filename, 'a', newline='') as f:  # Use 'a' to append results
-            records = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        with open(csv_filename, "a", newline="") as f:  # Use 'a' to append results
+            records = csv.writer(
+                f, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
+            )
 
             if f.tell() == 0:
                 records.writerow(["Model Name", "Similarity Score"])
 
-            records.writerow([model_name, similarity_scores]) 
-    
+            records.writerow([model_name, similarity_scores])
+
     def compute_augmented_similarities_for_all_images(self) -> dict:
         similarity_scores = {}
         all_augmented_images = {}
@@ -167,16 +181,14 @@ class RBenchmarking:
         self.visualize_all_images(all_augmented_images, similarity_scores)
 
         return similarity_scores
-    
-    #TODO crop image by sizes 
+
+    # TODO crop image by sizes
 
     def plot(self, sorted_results):
 
         filenames, scores = zip(*sorted_results)
 
-        plt.figure(
-            figsize=(20, max(14, len(filenames) * 0.5))
-        )  
+        plt.figure(figsize=(20, max(14, len(filenames) * 0.5)))
         bars = plt.barh(
             filenames,
             scores,
@@ -220,6 +232,7 @@ class RBenchmarking:
         logging.info(f"Saved in {save_path}" + f"/Dist_{self.model_name}.jpg")
         # plt.show()
 
+
 class Analyzer:
     def __init__(self, paths):
         self.paths = paths
@@ -232,11 +245,11 @@ class Analyzer:
         except Exception as e:
             logging.info(f"Error reading {path}: {e}")
             return None
-        
+
     def _plot(self):
         dfs = {path: self.read_csv(path) for path in self.paths}
-        dfs = {path: df for path, df in dfs.items() if df is not None}  
-        
+        dfs = {path: df for path, df in dfs.items() if df is not None}
+
         if not dfs:
             logging.info("No valid CSV files to analyze.")
             return
@@ -244,8 +257,8 @@ class Analyzer:
         plt.figure(figsize=(30, 20), dpi=200)
 
         num_csvs = len(dfs)
-        bar_width = 0.1  
-        index = np.arange(len(next(iter(dfs.values()))["Model Name"]))  
+        bar_width = 0.1
+        index = np.arange(len(next(iter(dfs.values()))["Model Name"]))
 
         for i, (file_path, df) in enumerate(dfs.items()):
             if "Model Name" not in df.columns or "Similarity Score" not in df.columns:
@@ -253,30 +266,43 @@ class Analyzer:
                 continue
 
             df = df.sort_values("Model Name")
-            
+
             model_names = df["Model Name"].values
             scores = df["Similarity Score"].values
 
             x_positions = index + (i * bar_width)
 
-            bars = plt.bar(x_positions, scores, width=bar_width, label=f"File {i+1}: {file_path.split('/')[-1]}", alpha=0.7)
+            bars = plt.bar(
+                x_positions,
+                scores,
+                width=bar_width,
+                label=f"File {i+1}: {file_path.split('/')[-1]}",
+                alpha=0.7,
+            )
 
             for bar in bars:
                 height = bar.get_height()
-                plt.text(bar.get_x() + bar.get_width() / 2, height + 0.01, f"{height:.2f}", ha='center', fontsize=10)
+                plt.text(
+                    bar.get_x() + bar.get_width() / 2,
+                    height + 0.01,
+                    f"{height:.2f}",
+                    ha="center",
+                    fontsize=10,
+                )
 
         plt.xlabel("Model Name")
         plt.ylabel("Similarity Score")
         plt.title("Model Similarity Scores from Multiple CSV Files")
-        plt.xticks(index + (num_csvs * bar_width) / 2, model_names, rotation=0, ha="right")
+        plt.xticks(
+            index + (num_csvs * bar_width) / 2, model_names, rotation=0, ha="right"
+        )
         plt.grid(visible=True)
         plt.legend()
         plt.tight_layout(pad=4)
-        plt.savefig(f"plots/barplots.jpg")     
-        plt.close()   
+        plt.savefig(f"plots/barplots.jpg")
+        plt.close()
         # plt.show()
 
-    
     def _avg_plot(self):
 
         valid_dfs = []
@@ -316,7 +342,7 @@ class Analyzer:
                 height + 0.01,
                 f"{height:.2f}",
                 ha="center",
-                fontsize=10
+                fontsize=10,
             )
 
         plt.xlabel("Model Name")
@@ -329,7 +355,6 @@ class Analyzer:
         plt.close()
         # plt.show()
 
-
     def __call__(self):
         self._plot()
         self._avg_plot()
@@ -340,22 +365,19 @@ if __name__ == "__main__":
 
     images_dir = "Test Images"
 
-
     for filename in os.listdir(images_dir):
         folder_path = os.path.join(images_dir, filename)
-
-
 
         for model_name in model_names:
             logging.info(f"Running model {model_name}")
 
             rb = RBenchmarking(
-                folder_path=folder_path,  
+                folder_path=folder_path,
                 model_name=model_name,
             )
 
             aug_results = rb.compute_augmented_similarities_for_all_images()
-            #Must be added here
+            # Must be added here
             sorted_aug_results = sorted(
                 aug_results.items(), key=lambda x: x[1], reverse=False
             )
@@ -364,19 +386,15 @@ if __name__ == "__main__":
 
             sum_score = [res[1] for res in sorted_aug_results]
 
-            average_score = sum(sum_score)/ len(sum_score)            
+            average_score = sum(sum_score) / len(sum_score)
             rb._record_to_csv(similarity_scores=average_score, model_name=model_name)
             # break
         # break
 
-
-
     analyzer = Analyzer(paths="plots")
 
-
-    csv_dir = "plots"  
-    csv_files = glob.glob(os.path.join(csv_dir, "**/*.csv"), recursive=True)  
+    csv_dir = "plots"
+    csv_files = glob.glob(os.path.join(csv_dir, "**/*.csv"), recursive=True)
 
     analyzer = Analyzer(paths=csv_files)
     analyzer()
-    
