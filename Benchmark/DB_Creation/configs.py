6bd37625f9743c7c5aee57a17b9a8398bd08f7be
GoshKolotyan <goshkolotyan@gmail.com>
2025-03-20 17:39:48 +0400

    adding GlobalConfigs

--- Benchmark/DB_Creation/configs.py
+++ Benchmark/DB_Creation/configs.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from typing import List, Dict
+from dataclasses import dataclass, field
 
 NUMBER_OF_TREES_FOR_ANNOY_INDEX = 200
 
@@ -23,6 +24,27 @@ MODEL_NAMES = [
 
 
 @dataclass(frozen=True)
+class GlobalConfigs:
+    MODEL_NAME: List[str] = field(default_factory=lambda: [
+        "swin_b", "swin_s", "swin_t", "swin_v2_b", "swin_v2_s", "swin_v2_t",
+        "dinov2_vits14_reg_lc", "dinov2_vitb14_reg_lc", "dinov2_vitl14_reg_lc", "dinov2_vitg14_reg_lc"
+    ])
+    OUTPUT_BASE_FOLDER: str = "../Dataset Selection/Images_frankwebb"
+    MAX_SIZE: int = 1024
+    NUMBER_OF_TREES_FOR_ANNOY_INDEX: int = 200
+
+    def __repr__(self):
+        return (f"GlobalConfigs(MODEL_NAME={self.MODEL_NAME}, "
+                f"OUTPUT_BASE_FOLDER='{self.OUTPUT_BASE_FOLDER}', "
+                f"MAX_SIZE={self.MAX_SIZE}, "
+                f"NUMBER_OF_TREES_FOR_ANNOY_INDEX={self.NUMBER_OF_TREES_FOR_ANNOY_INDEX})")
+
+global_config = GlobalConfigs()
+
+print(repr(global_config))  
+
+print("\n")
+@dataclass(frozen=True)
 class DBConfigs:
     NAME: str = "wcproject_v0"
     USER: str = "postgres"
@@ -33,7 +55,6 @@ class DBConfigs:
     def __repr__(self):
         return f"DBConfigs(NAME={self.NAME}, USER={self.USER}, PASSWORD={self.PASSWORD} HOST={self.HOST}, PORT={self.PORT})"
 
-# Creating an instance
 db_config = DBConfigs()
 
 
