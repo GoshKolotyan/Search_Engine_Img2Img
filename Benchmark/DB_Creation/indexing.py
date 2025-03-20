1e7a2eb699139ef58cf22585e6e3bb972b891a5c
GoshKolotyan <goshkolotyan@gmail.com>
2025-03-20 15:59:27 +0400

    adding loggs and more dynamic code

--- Benchmark/DB_Creation/indexing.py
+++ Benchmark/DB_Creation/indexing.py
@@ -25,50 +25,50 @@ class ModelDatabaseBuilder:
         num_trees: str = NUMBER_OF_TREES_FOR_ANNOY_INDEX,
     ):
         self.model_name = model_name
-        os.mkdir(self.model_name)
+        os.makedirs(self.model_name, exist_ok=True)
 
         self.metadata_path = self.model_name + "/features_metadata.pkl"
         self.index_path = self.model_name + "/image_search_index.ann"
-        # self.num_tree = num_trees
-        # self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        # self.base_dir = base_dir
-        # if "swin" in self.model_name:
-        #     self.model = getattr(models, self.model_name)(weights="IMAGENET1K_V1")
-        #     self.model = torch.nn.Sequential(*list(self.model.children())[:-1]).to(device=self.device)
-        #     self.model = self.model.to(self.device).eval()
+        self.num_tree = num_trees
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.base_dir = base_dir
+        if "swin" in self.model_name:
+            self.model = getattr(models, self.model_name)(weights="IMAGENET1K_V1")
+            self.model = torch.nn.Sequential(*list(self.model.children())[:-1]).to(device=self.device)
+            self.model = self.model.to(self.device).eval()
 
-        #     self.preprocess = transforms.Compose(
-        #         [
-        #             transforms.Resize((224, 224)),
-        #             transforms.ToTensor(),
-        #             transforms.Normalize(
-        #                 mean=[0.485, 0.456, 0.406],
-        #                 std=[0.229, 0.224, 0.225],
-        #             ),
-        #         ]
-        #     )
-        #     dummy_input = torch.zeros(1, 3, 224, 224)
-        #     with torch.no_grad():
-        #         feature_vector = self.model(dummy_input)
-        #     self.feature_dim = feature_vector.view(-1).shape[0]
+            self.preprocess = transforms.Compose(
+                [
+                    transforms.Resize((224, 224)),
+                    transforms.ToTensor(),
+                    transforms.Normalize(
+                        mean=[0.485, 0.456, 0.406],
+                        std=[0.229, 0.224, 0.225],
+                    ),
+                ]
+            )
+            dummy_input = torch.zeros(1, 3, 224, 224).to(self.device)
+            with torch.no_grad():
+                feature_vector = self.model(dummy_input)
+            self.feature_dim = feature_vector.view(-1).shape[0]
 
-        # elif "dino":
-        #     self.model = torch.hub.load("facebookresearch/dinov2", self.model_name)
-        #     self.model = self.model.to(self.device).eval()
-        #     self.preprocess = transforms.Compose(
-        #         [
-        #             transforms.Resize((518, 518)),  # Official DINOv2 input size
-        #             transforms.ToTensor(),
-        #             transforms.Normalize(
-        #                 mean=[0.485, 0.456, 0.406], 
-        #                 std=[0.229, 0.224, 0.225]
-        #             ),
-        #         ]
-        #     )
-        #     dummy_input = torch.zeros(1, 3, 518, 518)
-        #     with torch.no_grad():
-        #         feature_vector = self.model(dummy_input)
-        #     self.feature_dim = feature_vector.view(-1).shape[0]
+        elif "dino" in self.model_name:
+            self.model = torch.hub.load("facebookresearch/dinov2", self.model_name)
+            self.model = self.model.to(self.device).eval()
+            self.preprocess = transforms.Compose(
+                [
+                    transforms.Resize((518, 518)),  # Official DINOv2 input size
+                    transforms.ToTensor(),
+                    transforms.Normalize(
+                        mean=[0.485, 0.456, 0.406], 
+                        std=[0.229, 0.224, 0.225]
+                    ),
+                ]
+            )
+            dummy_input = torch.zeros(1, 3, 518, 518)
+            with torch.no_grad():
+                feature_vector = self.model(dummy_input)
+            self.feature_dim = feature_vector.view(-1).shape[0]
 
 
 
@@ -77,90 +77,93 @@ class ModelDatabaseBuilder:
 
         logging.info("Initilazting of DatabaseBuilder")
 
-    def extract_features(self, image: Image.Image) -> torch.Tensor:
+    def _computer_features_swin(self, image: Image.Image) -> np.ndarray:
         if image is None:
             return None
-        
-        image_tensor = self.processor(image).unsqueeze(0).to(self.device)
+        image_tensor = self.preprocess(image).unsqueeze(0).to(self.device)
         with torch.no_grad():
             features = self.model(image_tensor)
-        # L2 normalize
         features = features.view(features.size(0), -1)
-        features = features / torch.norm(features, p=2, dim=1, keepdim=True)
-        return features
-    def _compute_features_dino(self, image: Image.Image) -> torch.Tensor:
+        features = F.normalize(features, p=2, dim=1)
+        return features.cpu().numpy().flatten()
+
+    def _compute_features_dino(self, image: Image.Image) -> np.ndarray:
         if image is None:
             return None
         input_tensor = self.preprocess(image).unsqueeze(0).to(self.device)
         with torch.no_grad(), torch.autocast(device_type="cuda"):
             features = self.model.backbone.forward_features(input_tensor)
             embeddings = features["x_norm_clstoken"]
-        return F.normalize(embeddings, p=2, dim=-1).squeeze(0)
+        embeddings = F.normalize(embeddings, p=2, dim=-1)
+        return embeddings.cpu().numpy().flatten()
+
     
-    # def build_database_and_save(
-    #     self,
-    #     save=True,
-    # ):
-    #     """
-    #     Scans 'self.base_dir' for category subfolders, loads images, extracts features (+ flip augmentation),
-    #     stores them, and then optionally saves the metadata + builds the Annoy index.
-    #     """
-    #     for category in os.listdir(self.base_dir):
-    #         category_path = os.path.join(self.base_dir, category)
-    #         if os.path.isdir(category_path):
-    #             for filename in tqdm(
-    #                 os.listdir(category_path), desc=f"Processing {category}"
-    #             ):
-    #                 if filename.lower().endswith((".png", ".jpg", ".jpeg")):
-    #                     image_path = os.path.join(category_path, filename)
+    def build_database_and_save(
+        self,
+        save=True,
+    ):
+        """
+        Scans 'self.base_dir' for category subfolders, loads images, extracts features (+ flip augmentation),
+        stores them, and then optionally saves the metadata + builds the Annoy index.
+        """
+        for category in os.listdir(self.base_dir):
+            category_path = os.path.join(self.base_dir, category)
+            if os.path.isdir(category_path):
+                for filename in tqdm(
+                    os.listdir(category_path), desc=f"Processing {category}"
+                ):
+                    if filename.lower().endswith((".png", ".jpg", ".jpeg")):
+                        image_path = os.path.join(category_path, filename)
 
-    #                     image = Image.open(image_path).convert("RGB")
-    #                     print(filename)
-    #                     # Attempt to parse product_id from the filename (before first underscore)
-    #                     # product_id = int(filename.split("_")[0])
+                        image = Image.open(image_path).convert("RGB")
+                        print(filename)
+                        # Attempt to parse product_id from the filename (before first underscore)
+                        # product_id = int(filename.split("_")[0])
 
-    #                     # Extract features for original image
-    #                     original_features = self.extract_features(image)
-    #                     self.add_to_database(
-    #                         original_features,
-    #                         category,
-    #                         filename,
-    #                         image_path,
-    #                         # product_id,
-    #                     )
+                        # Extract features for original image
+                        original_features = self._computer_features_swin(image) if"swin" in self.model_name else self._compute_features_dino(image)
+                        
+                        self.add_to_database(
+                            original_features,
+                            category,
+                            filename,
+                            image_path,
+                            # product_id,
+                        )
 
-    #                     # Example data augmentation: horizontal flip
-    #                     augmented_versions = [("flipped", hflip(image))]
-    #                     for aug_suffix, aug_image in augmented_versions:
-    #                         aug_filename = f"{os.path.splitext(filename)[0]}_{aug_suffix}{os.path.splitext(filename)[1]}"
-    #                         aug_features = self.extract_features(aug_image)
-    #                         # For flips, we can keep the same product_id
-    #                         self.add_to_database(
-    #                             aug_features,
-    #                             category,
-    #                             aug_filename,
-    #                             image_path,
-    #                             # product_id,
-    #                         )
+                        # Example data augmentation: horizontal flip
+                        augmented_versions = [("flipped", hflip(image))]
+                        for aug_suffix, aug_image in augmented_versions:
+                            aug_filename = f"{os.path.splitext(filename)[0]}_{aug_suffix}{os.path.splitext(filename)[1]}"
+                            aug_features = self._computer_features_swin(aug_image) if "swin" in self.model_name else self._compute_features_dino(aug_image)
 
-    #     if save:
-    #         # Save features + metadata
-    #         with open(self.metadata_path, "wb") as f:
-    #             pickle.dump((self.database_features, self.database_metadata), f)
+                            # For flips, we can keep the same product_id
+                            self.add_to_database(
+                                aug_features,
+                                category,
+                                aug_filename,
+                                image_path,
+                                # product_id,
+                            )
 
-    #         logging.info(f"Metadata save to {self.metadata_path}")
+        if save:
+            # Save features + metadata
+            with open(self.metadata_path, "wb") as f:
+                pickle.dump((self.database_features, self.database_metadata), f)
 
-    #         # Build Annoy index
-    #         logging.info("Building Annoy index")
-    #         annoy_index = AnnoyIndex(self.feature_dim, "angular")
+            logging.info(f"Metadata save to {self.metadata_path}")
 
-    #         for i, features in enumerate(self.database_features):
-    #             annoy_index.add_item(i, features)
+            # Build Annoy index
+            logging.info("Building Annoy index")
+            annoy_index = AnnoyIndex(self.feature_dim, "angular")
 
-    #         annoy_index.build(self.num_tree)
-    #         annoy_index.save(self.index_path)
+            for i, features in enumerate(self.database_features):
+                annoy_index.add_item(i, features)
 
-    #         logging.info(f"Annoy index saved to {self.index_path}")
+            annoy_index.build(self.num_tree)
+            annoy_index.save(self.index_path)
+
+            logging.info(f"Annoy index saved to {self.index_path}")
 
     def add_to_database(
         self,
--- Benchmark/DB_Creation/main.py
+++ Benchmark/DB_Creation/main.py
@@ -1,21 +1,37 @@
-
+import logging
+import os
 from configs import OUTPUT_BASE_FOLDER, MODEL_NAMES
-
 from indexing import ModelDatabaseBuilder
 from typing import List
 
-def main(output_base_folder: str, Model_Names: List[str]) -> None:
+# Create logs folder if it doesn't exist
+log_folder = 'logs'
+os.makedirs(log_folder, exist_ok=True)
+
+# Configure logging to save to file
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler(os.path.join(log_folder, 'indexing.log')),
+        logging.StreamHandler()
+    ]
+)
+
+def main(output_base_folder: str, model_names: List[str]) -> None:
     """
     Read Data -> Load Model -> Segment Element -> Keep in DB in our selected method.
     """
-    print([name for name in Model_Names])
-    for name  in Model_Names:
+    logging.info(f"Starting indexing for models: {', '.join(model_names)}")
+
+    for name in model_names:
+        logging.info(f"Initializing ModelDatabaseBuilder for model: {name}")
         index_creator = ModelDatabaseBuilder(output_base_folder, model_name=name)
+        logging.info(f"Building and saving database for model: {name}")
+        index_creator.build_database_and_save()
+        logging.info(f"Completed indexing for model: {name}")
 
-
-    # index_creator.build_database_and_save()
-    return None
-
+    logging.info("All models have been successfully processed.")
 
 if __name__ == "__main__":
-    main(output_base_folder=OUTPUT_BASE_FOLDER, Model_Names= MODEL_NAMES)
+    main(output_base_folder=OUTPUT_BASE_FOLDER, model_names=MODEL_NAMES)
